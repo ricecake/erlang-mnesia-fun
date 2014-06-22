03727deb8a43a5ba7315ad764aa51ea3aaf2d309
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-22 17:47:44 -0400

    config

--- fragmentation/src/frag_app.erl
+++ fragmentation/src/frag_app.erl
@@ -10,6 +10,10 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+        mnesia:create_schema([node()]),
+        mnesia:start(),
+        mnesia:create_table(dict, [{attributes,[prim_key,prim_val]}, {disc_copies, [node()]},
+                                   {frag_properties, [{n_fragments, 64}]}]),
     frag_sup:start_link().
 
 stop(_State) ->
